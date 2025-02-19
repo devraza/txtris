ad5095aae0e9e778e6e1d3f038ad012152594fc6
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-02-19 23:01:47 +0000

    feat: blank 'Profile' section and more flair to the UI

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,17 +4,25 @@ use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
     layout::{Constraint, Flex, Layout, Rect},
-    style::{Modifier, Style, palette::tailwind::*},
-    text::Line,
-    widgets::{Block, Borders, HighlightSpacing, List, ListState, StatefulWidget, Widget},
+    style::{Modifier, Style, palette::tailwind::*, Stylize},
+    text::{Line, Span},
+    widgets::{Block, Borders, HighlightSpacing, List, ListState, StatefulWidget, Widget, Paragraph},
 };
 
-const MENU_HEADER_STYLE: Style = Style::new()
+const GAME_HEADER_STYLE: Style = Style::new()
     .fg(ZINC.c100)
     .bg(BLUE.c600)
     .add_modifier(Modifier::BOLD);
+const PROFILE_HEADER_STYLE: Style = Style::new()
+    .fg(ZINC.c100)
+    .bg(VIOLET.c600)
+    .add_modifier(Modifier::BOLD);
+
 const HEADER_STYLE: Style = Style::new().fg(ROSE.c500).add_modifier(Modifier::BOLD);
 const SELECTED_STYLE: Style = Style::new().bg(ZINC.c700).add_modifier(Modifier::BOLD);
+
+const FOOTER_LEFT_STYLE: Style = Style::new().fg(PURPLE.c400).add_modifier(Modifier::BOLD);
+const FOOTER_RIGHT_STYLE: Style = Style::new().fg(LIME.c400);
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -46,22 +54,38 @@ impl Default for App {
     fn default() -> Self {
         Self {
             should_exit: false,
-            menu: MenuList::from_iter(["40L", "Blitz", "txLadder"]),
+            menu: MenuList::from_iter(["40L", "Blitz", "txLadder", "Config"]),
         }
     }
 }
 
 impl App {
-    fn render_header(area: Rect, buf: &mut Buffer) {
+    fn render_header(&mut self, area: Rect, buf: &mut Buffer) {
         Block::new()
             .title(Line::raw("  txtris  ").centered().style(HEADER_STYLE))
             .borders(Borders::TOP)
             .render(area, buf);
     }
 
+    fn render_footer(&mut self, area: Rect, buf: &mut Buffer) {
+        Block::new()
+            .title(
+                Line::raw(" Atiran © 2025 ")
+                    .left_aligned()
+                    .style(FOOTER_LEFT_STYLE),
+            )
+            .title(
+                Line::raw(format!("v{} ", env!("CARGO_PKG_VERSION")))
+                    .right_aligned()
+                    .style(FOOTER_RIGHT_STYLE),
+            )
+            .borders(Borders::BOTTOM)
+            .render(area, buf);
+    }
+
     fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
         let block = Block::new()
-            .title(Line::raw(" Menu ").centered().style(MENU_HEADER_STYLE))
+            .title(Line::raw(" Game ").centered().style(GAME_HEADER_STYLE))
             .borders(Borders::ALL);
 
         let items: Vec<&'static str> = self.menu.items.clone();
@@ -73,6 +97,32 @@ impl App {
             .highlight_spacing(HighlightSpacing::Always);
 
         StatefulWidget::render(list, area, buf, &mut self.menu.state);
+    }
+
+    fn render_profile(&mut self, area: Rect, buf: &mut Buffer) {
+        let block = Block::new()
+            .title(Line::raw(" Profile ").centered().style(PROFILE_HEADER_STYLE))
+            .borders(Borders::ALL);
+
+        let text = vec![
+            Line::from(Span::styled("<Username>", Style::new().bold().underlined())),
+            Line::from(vec![
+                Span::styled("40L: ", Style::new().blue()),
+                Span::raw("N/A"),
+            ]),
+            Line::from(vec![
+                Span::styled("Blitz: ", Style::new().green()),
+                Span::raw("N/A"),
+            ]),
+            Line::from(vec![
+                Span::styled("txLadder: ", Style::new().magenta()),
+                Span::raw("N/A"),
+            ]),
+        ];
+
+        Paragraph::new(text)
+            .block(block)
+            .render(area, buf);
     }
 
     fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
@@ -120,12 +170,22 @@ fn center(area: Rect, horizontal: Constraint, vertical: Constraint) -> Rect {
 
 impl Widget for &mut App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let [header_area, main_area] =
-            Layout::vertical([Constraint::Length(2), Constraint::Fill(1)]).areas(area);
+        let [header_area, main_area, footer_area] = Layout::vertical([
+            Constraint::Length(1),
+            Constraint::Fill(1),
+            Constraint::Length(2),
+        ])
+        .areas(area);
 
-        let list_area = center(main_area, Constraint::Length(30), Constraint::Length(5));
+        let center_area = center(main_area, Constraint::Length(50), Constraint::Percentage(60));
 
-        App::render_header(header_area, buf);
-        self.render_list(list_area, buf);
+        let [profile_area, list_outer_area] =
+            Layout::vertical([Constraint::Percentage(50), Constraint::Percentage(50)])
+                .areas(center_area);
+
+        self.render_header(header_area, buf);
+        self.render_profile(profile_area, buf);
+        self.render_list(list_outer_area, buf);
+        self.render_footer(footer_area, buf);
     }
 }
